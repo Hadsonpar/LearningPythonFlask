fcf3f71d0490aa6a937faf2628ad2505f491bc17
Hadsonpar <hadson1@gmail.com>
2024-02-03 07:37:32 -0500

    import markupsafe and apply escape

--- 02.html-escape-project/app.py
+++ 02.html-escape-project/app.py
@@ -1,17 +1,13 @@
 from flask import Flask # import to initialize
+from markupsafe import escape # import markupsafe and apply escape
 
 # Constructor, the path of the correct module 
 app = Flask(__name__) # initial Object
 @app.route('/')
 
-def helloworld():
-    #return 'Hola mundo desde Python y Flask'
-    return 'Debug project Python y Flask'
-
-# Modified program for see debug incorport Werkzeug
 @app.route('/<message>')
 def fmessage(message):    
-    return f'Your message ir {int(message)}'
+    return f'Your message ir: {escape(message)}'
 
 if __name__ == '__main__':
     app.run(debug=True) # debug project
